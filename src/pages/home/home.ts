e2b431c065d18bd88bc59ba83d4d1687cf6bae90
Agnes Konradsdottir <agneskonradsdottir@gmail.com>
2018-05-17 13:39:31 +0200

    initialize user within constructor function

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,7 +8,9 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
   user: any = {};
 
-  constructor(public navCtrl: NavController) {} 
+  constructor(public navCtrl: NavController) {
+    this.user = { distance: 1000, age: 20 };
+  } 
   calculate() {
     console.log(this.user);
   }
