d031eb6ce5f3d56b1c8287629384276f78cd191a
Agnes Konradsdottir <agneskonradsdottir@gmail.com>
2018-05-18 15:21:23 +0200

    Import performance data provider, modify calculate user

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { PersonProvider } from '../../providers/person/person';
+import { PerformanceDataProvider } from '../../providers/performance-data/performance-data';
 
 @Component({
   selector: 'page-home',
@@ -11,16 +12,19 @@ export class HomePage {
 
   constructor(
     public navCtrl: NavController,
-    public person: PersonProvider
+    public person: PersonProvider,
+    public performanceData: PerformanceDataProvider
   ) {
     this.user = { distance: 1000, age: 20, gender: 'female' };
   }
 
-  calculate() {
+  calculate(user) {
     this.person.age = this.user.age;
     this.person.gender = this.user.gender;
-
     this.person.doAssessment(this.user.distance);
-    console.log(this.person.assessmentMessage);
+    this.performanceData
+      .saveData({ performance_data: { data: { message: this.person.assessmentMessage } } })
+      .subscribe(data => console.log(data));
+   
   }
 }
